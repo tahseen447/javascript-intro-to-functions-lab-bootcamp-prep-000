e6b76669e29621723d12b2854a2a422bcd3030b1
tahseen447 <tahseenfatima1986@gmail.com>
2017-09-29 08:15:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ function whisper(string)
 }
 
 function logShout(string)
-{}
+{
+  console.log(string.toUpperCase()
+}
 function logWhisper(string)
-{}
+{
+  console.log(string.toLowerCase()
+}
 function sayHiToGrandma(string)
-{}
+{
+  if(string.toUpperCase()=== string)
+  return 'YES INDEED!';
+  else if(string.toLowerCase()=== string)
+  return 'I can\'t hear you!'
+  else if (string == 'I love you, Grandma.')
+  {
+    return 'I love you, too.';
+  }
+}
